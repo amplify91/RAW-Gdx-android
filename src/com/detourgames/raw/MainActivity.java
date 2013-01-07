408efb90b28536e674dea7871e851aa11df1c998
Clayton Langford <amplify91@gmail.com>
2013-01-07 15:33:04 -0500

    Trying to get working for OUYA.

--- src/com/detourgames/raw/MainActivity.java
+++ src/com/detourgames/raw/MainActivity.java
@@ -1,13 +1,18 @@
 package com.detourgames.raw;
 
+import tv.ouya.console.api.OuyaFacade;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class MainActivity extends AndroidApplication {
+	
+	public static final String OUYA_DEV_ID = "7d711435-a3f5-4f4b-b9e9-70f101d43df0";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	//OuyaFacade.getInstance().init(this, OUYA_DEV_ID);
         super.onCreate(savedInstanceState);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
@@ -16,5 +21,12 @@ public class MainActivity extends AndroidApplication {
         cfg.useCompass = false;
         
         initialize(GameRAW.getGameRAW(), cfg);
+        
+    }
+    
+    @Override
+    protected void onDestroy() {
+        //OuyaFacade.getInstance().shutdown();
+        super.onDestroy();
     }
 }
